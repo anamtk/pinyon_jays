ee5379cb6512d4e8e85346a85a6a0c064952a71b
Ana Miller-ter Kuile <ana.miller.ter.kuile@gmail.com>
2024-07-16 15:24:01 -0700

    script to download from prism

---
+++ code/data_prep/spatial/00_importprism.R
@@ -0,0 +1,21 @@
+library(prism)
+library(terra)
+library(here)
+library(tidyverse)
+
+# prism_set_dl_dir(path = 
+#                    here('data',
+#                         'spatial_data',
+#                         'prism_temp'), create = TRUE)
+# 
+# get_prism_annual(type = "tmean", year = 2005:2022, keepZip = FALSE)
+
+# prism_set_dl_dir(path = 
+#                    here('data',
+#                         'spatial_data',
+#                         'prism_ppt'), create = TRUE)
+# 
+# get_prism_annual(type = "ppt", year = 2005:2022, keepZip = FALSE)
+
+
+
