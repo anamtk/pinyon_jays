98660b2593e975623f6cca1773a85d326f1aee51
Ana Miller-ter Kuile <ana.miller.ter.kuile@gmail.com>
2025-05-14 15:49:44 -0700

    all current mosnoon stuff

--- monsoon/ebird/nospuncert/inputs/covariate_samples.R
+++ monsoon/ebird/nospuncert/inputs/covariate_samples.R
@@ -19,38 +19,11 @@ for(i in package.list){library(i, character.only = T)}
 
 mod <- readRDS("/scratch/atm234/pinyon_jays/ebird/nospuncert/outputs/ebird_abund_model2_checkN.RDS")
 
-# Update for residuals ----------------------------------------------------
-
-parms <- c("resid")
-
-mod2 <- update(mod,
-               parameters.to.save = parms,
-               n.iter = 3000)
-
-sum2 <- summary(mod2$samples)
-
-saveRDS(sum2, file ="/scratch/atm234/pinyon_jays/ebird/nospuncert/outputs/ebird_abund_model_residuals.RDS")
-
-# Replicated data ---------------------------------------------------------
-
-parms2 <- c('ebird.count.rep')
-
-mod3 <- update(mod,
-               parameters.to.save = parms2,
-               n.iter = 3000)
-
-sum3 <- summary(mod3$samples)
-
-saveRDS(sum3, file ="/scratch/atm234/pinyon_jays/ebird/nospuncert/outputs/ebird_abund_model_yrep.RDS")
-
-(end.time <- Sys.time())
-
-
 # samples of yrep ---------------------------------------------------------
 
 mod4 <- update(mod,
-               parameters.to.save = parms2,
-               n.iter = 350)
+               parameters.to.save = 'ebird.count.rep',
+               n.iter = 3000)
 
 saveRDS(mod4$samples, file ="/scratch/atm234/pinyon_jays/ebird/nospuncert/outputs/ebird_abund_model_yrepsamples.RDS")
 
@@ -69,20 +42,8 @@ parms3 <- c('a0',
 
 mod5 <- update(mod,
                parameters.to.save = parms3,
-               n.iter = 350)
-
-saveRDS(mod5$samples, file ="/scratch/atm234/pinyon_jays/ebird/nospuncert/outputs/ebird_abund_model_covariate_effect_samples.RDS")
-
-(end.time <- Sys.time())
-
-
-# RMSE --------------------------------------------------------------------
-
-
-mod6 <- update(mod,
-               parameters.to.save = 'RMSE',
                n.iter = 3000)
 
-saveRDS(mod6$samples, file ="/scratch/atm234/pinyon_jays/ebird/nospuncert/outputs/ebird_abund_model_RMSE_samples.RDS")
+saveRDS(mod5$samples, file ="/scratch/atm234/pinyon_jays/ebird/nospuncert/outputs/ebird_abund_model_covariate_effect_samples.RDS")
 
 (end.time <- Sys.time())
